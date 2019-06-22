fb3be45061fe0ab36c204fdbe5e8fa29bf8baa37
Lucasedu191 <lucasatletico@hotmail.com>
2019-06-21 22:52:53 -0300

    Criando metodo para verificar campo, categoria, se existe item nela e validação de exclusão

--- SistemaVendas/frm_Categoria.cs
+++ SistemaVendas/frm_Categoria.cs
@@ -30,23 +30,64 @@ namespace SistemaVendas
 
         private void btnCadastrar_Click(object sender, EventArgs e)
         {
-            this.categoriaBindingSource.EndEdit();
-            DataContextFactory.DataContext.SubmitChanges();
-            MessageBox.Show("Cadastro Realizado com sucesso!");
+            if (this.valida())
+            {
+                this.categoriaBindingSource.EndEdit();
+                DataContextFactory.DataContext.SubmitChanges();
+                MessageBox.Show("Cadastro Realizado com sucesso!");            
+            }
 
+        }
+
+        private bool valida()
+        {
+            if(txtCategoria.Text.Trim()== string.Empty)
+            {
+                MessageBox.Show("O campo de categoria é obrigatório");
+                txtCategoria.Focus();
+                return false;
+            }
+            return true;
         }
 
         private void tnExcluir_Click(object sender, EventArgs e)
         {
-            this.categoriaBindingSource.RemoveCurrent();
-            DataContextFactory.DataContext.SubmitChanges();
-            MessageBox.Show("Categoria excluída com sucesso!");
+            if (MessageBox.Show("Tem certeza que deseja excluir?", "Confirmação", MessageBoxButtons.YesNo, MessageBoxIcon.Question) == DialogResult.Yes)
+            {
+                if (this.CategoriaPossuiProdutos(this.categoriaAtual))
+                    MessageBox.Show("Você não pode excluir essa categoria, porque existe produtos nela");
+                else
+                {
+                    this.categoriaBindingSource.RemoveCurrent();
+                    DataContextFactory.DataContext.SubmitChanges();
+                    MessageBox.Show("Categoria excluída com sucesso!");
+                }
+                
+            }
+            
         }
 
         private void btnCancelar_Click(object sender, EventArgs e)
         {
             this.categoriaBindingSource.CancelEdit();
             
+        }
+
+        public Categoria categoriaAtual
+        {
+            get
+            {
+                return (Categoria)this.categoriaBindingSource.Current;
+            }
+        }
+
+        private bool CategoriaPossuiProdutos(Categoria categoria)
+        {
+            var produtos = DataContextFactory.DataContext.Produtos.Where(x => x.CodigoCategoria == categoria.Codigo);
+            if (produtos.Count() > 0)
+                return true;
+            else
+                return false;
         }
     }
 }
